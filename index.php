5f8ae9665ae12e3cbd1154b7ddb17509856a0693
yayimahuman <donha@utschools.ca>
2015-11-19 21:26:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en" class="off-canvas">
 <head>
+<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+<script type="text/javascript">stLight.options({publisher: "042fe7c8-61a0-432b-9f9a-3808a2ef5415", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 <link rel="icon" href="/favicon.ico" type="image/x-icon">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="description" content="Search over 12 million books and find your reading time for each one with How Long to Read.">
 <meta charset="UTF-8">
 <title>How Long to Read</title>
 <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
@@ -15,65 +18,31 @@
 <link href='//fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 <link href="//fonts.googleapis.com/css?family=RobotoDraft:100,300,400,500,700" rel=
     "stylesheet">
-<script type="text/javascript">
-    $(function () {
-      $(document).ready(function(){
-        var Size = screen.height;
-			if(Size == 600)
-				{
-					  $(".mid_container").css('min-height',420);
-				}
-			if(Size == 768)
-				{
-					  $(".mid_container").css('min-height',664);
-				}
-			if(Size == 800)
-				{
-					  $(".mid_container").css('min-height',543);
-				}
-			if(Size == 832)
-				{
-					  $(".mid_container").css('min-height',563);
-				}	
-			if(Size == 900)
-				{
-					  $(".mid_container").css('min-height',800);
-				}	
-			if(Size == 1050)
-				{
-					  $(".mid_container").css('min-height',949);
-				}
-			if(Size == 1080)
-				{
-					  $(".mid_container").css('min-height',982);
-				}	
-				
-      });
-    });
-  </script>
+<style>
+	background:#0CD39A;
+</style>
 </head>
+
 <body class="fs12 page-home ">
 <div id="page-container">
 <?php include_once('header.php'); ?>
 <?php include_once("analyticstracking.php") ?>
 </div>
-<section class="pav-showcase mid_container" id="pavo-showcase">
-   <div class="container" >
-	   <div class="row-fluid" style="margin-bottom:20px; text-align:center;">
-		   <div class="span12">
-			   <img width="200px" src="images/logo.png">
-			   <div class="spacer10"></div>
-			   <h2 style="color:white;">How Long to Read</h2>
-			   <div class="spacer10"></div>
-			   <form action="search_result.php" method="get">
-			   <input type="text" class="span8" name="search_keyword" id="search_keyword" autofocus="autofocus" size="21" maxlength="120" style="margin:0; height:43px;" placeholder="Search over 12 million books">
-			   <input type="submit" class="btn btn-default btn-large" value="Search">
-			   </form>
-			   <div style="margin-top:60px;"> <p style="margin-top:20px;text-align:center">Search over 12 million books and find your reading time for each one.</p></div>
-		   </div>
-	   </div>
-   </div>
-</section>
-	<div class="myFooter">
-			<?php include_once('footer.php'); ?>
-	</div>	
+<div class="container" >
+	<div class="row-fluid" style="margin-bottom:20px; text-align:center;">
+		<div class="span12">
+			<img width="400px" src="images/logo.png">
+			<div class="spacer10"></div>
+			<h2 style="color:white;">How Long to Read</h2>
+			<div class="spacer10"></div>
+			<form action="search_result.php" method="get">
+			<input type="text" class="span8" name="search_keyword" id="search_keyword" autofocus="autofocus" size="21" maxlength="120" style="margin:0; height:43px;" placeholder="Search over 12 million books">
+			<input type="submit" class="btn btn-default btn-large" value="Search">
+			</form>
+			<div style="margin-top:60px;"> <p style="margin-top:20px;text-align:center">Search over 12 million books and find your reading time for each one.</p></div>
+		 </div>
+	</div>
+</div>
+<div class="myFooter">
+	<?php include_once('footer.php'); ?>
+</div>	
