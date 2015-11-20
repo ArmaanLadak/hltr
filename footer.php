1ecd856f9aba520846c9a55f24f729cde723e03b
yayimahuman <donha@utschools.ca>
2015-11-19 19:21:54 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<div id="powered">
+	<div class="container">
+		<div class="copyright pull-left">  &copy; <?php echo date("Y"); ?>. Developed by Alexander Thorburn-Winsor </div> 
+		<div class="pull-right">
+		<br>
+		<span class='st_facebook_hcount' displayText='Facebook'></span>
+<span class='st_twitter_hcount' displayText='Tweet'></span>
+<span class='st_pinterest_hcount' displayText='Pinterest'></span>
+</div>
+		<!--<div class="pull-centre"><a style="color:white;" href="http://www.howlongtoreadthis.com/disclaimer.php"><span class="menu-title">Disclaimer</span></a></div>-->	
+	</div>
+	
+</div>
+</body>
+</html>
