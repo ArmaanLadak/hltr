2564a482aabc997d34904aa6013a07fdc4682094
yayimahuman <donha@utschools.ca>
2015-11-19 23:13:47 -0500

    edited form

--- index.php
+++ index.php
@@ -37,9 +37,11 @@ body{
 			<h2 style="color:white;">How Long to Read</h2>
 			<div class="spacer10"></div>
 			<form action="search_result.php" method="get">
-			<input type="text" class="span8" name="search_keyword" id="search_keyword" autofocus="autofocus" size="21" maxlength="120" style="margin:0; height:43px;" placeholder="Search over 12 million books">
-			<input type="submit" class="btn btn-default btn-large" value="Search">
-			</form>
+    				<div class="form-group">
+				<input type="text" class="form-control" name="search_keyword" id="search_keyword" style="height:45px" autofocus="autofocus" maxlength="120" placeholder="Search over 12 million books">
+    				</div>
+    				<center><input type="submit" class="btn btn-default btn-large" value="Search"></center>
+  			</form>
 			<div style="margin-top:60px;"> <p style="margin-top:20px;text-align:center">Search over 12 million books and find your reading time for each one.</p></div>
 		 </div>
 	</div>
