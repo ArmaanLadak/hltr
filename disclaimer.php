43caece388a59ca890ee22b5f40a9955db30147c
yayimahuman <donha@utschools.ca>
2015-11-19 19:21:41 -0500

    Create disclaimer.php

---
+++ disclaimer.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en" class="off-canvas">
+<head>
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta charset="UTF-8">
+<title>Disclaimer</title>
+<link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="./css/stylesheet.css">
+<link rel="stylesheet" type="text/css" href="./css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css" href="./css/theme-responsive.css">
+<script type="text/javascript" src="./js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+<link href='//fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+</head>
+<body class="fs12 page-home ">
+<div id="page-container">
+<?php include_once('header.php'); ?>
+</div>
+<section class="pav-showcase mid_container" id="pavo-showcase">
+   <div class="container" >
+   <div class="row-fluid" style="margin-bottom:20px; min-height:500px;">
+   <div class="span12">
+	<div style="font-weight: bold; font-size: 25px; color:#ecf0f1;" class="span12 title">
+	Disclaimer
+	</div>
+	<div class="span12 title">
+	<br>
+	<div class="well well-small">
+	<p style="text-align:justify;font-size:14px; color:#34495e; line-height:2">
+	How Long to Read makes money by providing links to buy books on Amazon and from other booksellers. When you want to buy a book that you've seen on our website and click on an Amazon link, we get a small commission if you buy the book, and it costs you nothing extra. This helps us keep the site up and running, and makes sure that we never have to sell your data to advertisers, along with providing easy access to the best prices on the web to buy books.
+	</p>
+	</div>
+	</div>
+   </div>
+   </div>
+   </div>
+</section>
+<?php include_once('footer.php'); ?>
