09c2a853ba4cd6fa845eb578acc3a20c99c84259
yayimahuman <donha@utschools.ca>
2015-11-19 19:16:17 -0500

    Create blog.php

---
+++ blog.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en" class="off-canvas">
+<head>
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta charset="UTF-8">
+<title>Popular Books</title>
+<link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="./css/stylesheet.css">
+<link rel="stylesheet" type="text/css" href="./css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css" href="./css/theme-responsive.css">
+<script type="text/javascript" src="./js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+<link href='//fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+<script type="text/javascript">
+    $(function () {
+      $(document).ready(function(){
+        var Size = screen.height;
+			if(Size == 600)
+				{
+					  $(".mid_container").css('min-height',420);
+				}
+			if(Size == 768)
+				{
+					  $(".mid_container").css('min-height',664);
+				}
+			if(Size == 800)
+				{
+					  $(".mid_container").css('min-height',543);
+				}
+			if(Size == 832)
+				{
+					  $(".mid_container").css('min-height',563);
+				}	
+			if(Size == 900)
+				{
+					  $(".mid_container").css('min-height',800);
+				}	
+			if(Size == 1050)
+				{
+					  $(".mid_container").css('min-height',949);
+				}
+			if(Size == 1080)
+				{
+					  $(".mid_container").css('min-height',982);
+				}	
+				
+      });
+    });
+  </script>
+</head>
+<body class="fs12 page-home ">
+<div id="page-container">
+<?php include_once('header.php'); ?>
+</div>
+<section class="pav-showcase mid_container" id="pavo-showcase">
+   <div class="container" >
+   <div class="row-fluid" style="margin-bottom:20px; min-height:500px;">
+   <div class="span12">
+	<div style="font-weight: bold; font-size: 25px;" class="span12 title">
+	Book Lists
+	</div>
+	<div class="span12 title">
+	<p style="text-align:justify;font-size:18px; color:#34495e; line-height:2">
+	<div class="well well-large">
+	<h2><b>Popular Books</b></h2>
+	<p>How Long to Read is a book search engine that helps you find out how long it will take to read books and provide reading time data that is tailored to you. With our simple WPM (words per minute) test you can find out how long it will take you to read almost anything, and also use our search engine to find books that will fit the time you have to read.
+</p>	
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0553593714&ean=9780553593716"><h4>How Long to Read a Game of Thrones</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/5163N91r6lL.jpg">
+		<p>From a master of contemporary fantasy comes the first novel of a landmark series unlike any you’ve ever read before. With A Game of Thrones, George R. R. Martin has launched a genuine masterpiece, bringing together the best the genre has to offer. Mystery, intrigue, romance, and adventure fill the pages of this magnificent saga, the first volume in an epic series sure to delight fantasy fans everywhere.
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=059035342X&ean=9780439708180"><h4>How Long to Read Harry Potter and the Sorcerer's Stone</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/51MU5VilKpL.jpg">
+		<p>Harry Potter has no idea how famous he is. That's because he's being raised by his miserable aunt and uncle who are terrified Harry will learn that he's really a wizard, just as his parents were. But everything changes when Harry is summoned to attend an infamous school for wizards, and he begins to discover some clues about his illustrious birthright. From the surprising way he is greeted by a lovable giant, to the unique curriculum and colorful faculty at his unusual school, Harry finds himself drawn deep inside a mystical world he never knew existed and closer to his own noble destiny.....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0553418025&ean=9780553418026"><h4>How Long to Read The Martian</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/41-YkFaghDL.jpg">
+		<p>Six days ago, astronaut Mark Watney became one of the first people to walk on Mars. 
+
+Now, he's sure he'll be the first person to die there.
+
+After a dust storm nearly kills him and forces his crew to evacuate while thinking him dead, Mark finds himself stranded and completely alone with no way to even signal Earth that he’s alive—and even if he could get word out, his supplies would be gone long before a rescue could arrive. 
+
+Chances are, though, he won't have time to starve to death. The damaged machinery, unforgiving environment, or plain-old "human error" are much more likely to kill him first. 
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=014241493X&ean=9780142414934"><h4>How Long to Read Paper Towns</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/51hgkNew%2BXL.jpg">
+		<p>When Margo Roth Spiegelman beckons Quentin Jacobsen in the middle of the night—dressed like a ninja and plotting an ingenious campaign of revenge—he follows her. Margo’s always planned extravagantly, and, until now, she’s always planned solo. After a lifetime of loving Margo from afar, things are finally looking up for Q . . . until day breaks and she has vanished. Always an enigma, Margo has now become a mystery. But there are clu....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0345803485&ean=9780345803481"><h4>How Long to Read Fifty Shades of Grey</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/51nJJ09ZpRL.jpg">
+		<p>When literature student Anastasia Steele goes to interview young entrepreneur Christian Grey, she encounters a man who is beautiful, brilliant, and intimidating. The unworldly, innocent Ana is startled to realize she wants this man and, despite his enigmatic reserve, finds she is desperate to get close to him. Unable to resist Ana’s quiet beauty, wit, and independent spirit, Grey admits he wants her, too—but on his own terms.
+ 
+Shocked yet thrilled by Grey’s singular erotic tastes, Ana hesitates. For all the trappings of success—his multinational businesses, his vast wealth, his loving family—Grey is a man tormented....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0062387243&ean=9780062387240"><h4>How Long to Read Divergent (Divergent Series)</h4></a>
+
+<img src="http://ecx.images-amazon.com/images/I/51PLHsAVRkL.jpg">		
+<p>This first book in Veronica Roth's #1 New York Times bestselling Divergent trilogy is the novel that inspired the major motion picture starring Shailene Woodley, Theo James, and Kate Winslet. This dystopian series set in a futuristic Chicago has captured the hearts of millions of teen and adult readers.
+Perfect for fans of the Hunger Games and Maze Runner series, Divergent and its sequels, Insurgent and Allegiant—plus Four: A Divergent Collection, four stories told from the perspective of the character Tobias—are the gripping story of a dystopian world transformed by courage, self-sacrifice, and love. Fans of the Divergent movie will find the book ....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=1455520632&ean=9781455520633"><h4>How Long to Read The Longest Ride</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/51xUSBhKuqL.jpg">
+		<p>In the tradition of his beloved first novel, The Notebook, #1 New York Times bestselling author Nicholas Sparks returns with the remarkable story of two couples whose lives intersect in profound and surprising ways.
+
+Ira Levinson is in trouble. Ninety-one years old and stranded and injured after a car crash, he struggles to retain consciousness until a blurry image materializes beside him: his beloved wife Ruth, who passed away nine years ago. Urging him to hang on, she forces him to remain alert by recounting the stories of their lifetime together - how they met, the precious paintings they collected together, the dark days of WWII and its effect on them and their families. Ira knows that Ruth can't ....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0060935464&ean=9780060935467"><h4>How Long to Read To Kill a Mockingbird</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/51TtWX6%2B0GL.jpg">
+		<p>Description</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0451524934&ean=9780451524935"><h4>How Long to Read 1984</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/31twj9hz1kL.jpg">
+		<p>View our feature on George Orwell’s 1984.Written in 1948, 1984 was George Orwell’s chilling prophecy about the future. And while 1984 has come and gone, Orwell’s narrative is timelier than ever. 1984 presents a startling and haunting vision of the world, so powerful that it is completely convincing from start to finish. No one can deny the power of this novel, its hold on the imaginations of multiple generations of readers, or the resiliency of its admonitions—a legacy that seems only to grow with the passage of time.....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0385738765&ean=9780385738767"><h4>How Long to Read The Scorched Earth</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/518YN1zzOjL.jpg">
+		<p>Read the second book in the #1 New York Times bestselling Maze Runner series that is soon to be a motion picture, hitting theaters September 18, 2015, and is perfect for fans of The Hunger Games and Divergent. The first book, The Maze Runner, is now a movie featuring the star of MTV's Teen Wolf, Dylan O’Brien; Kaya Scodelario; Aml Ameen; Will Poulter; and Thomas Brodie-Sangster! Also look for James Dashner’s newest novels, The Eye of Minds and The Rule of Thoughts, the first two books in the Mortality Doctrine series.
+ 
+Solving the Maze was supposed to be the end. 
+ 
+Thomas was sure that escape from the Maze would mean freedom for him and the ....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0439023521&ean=9780439023528"><h4>How Long to Read The Hunger Games</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/41bOj-am1RL.jpg">
+		<p>In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. Long ago the districts waged war on the Capitol and were defeated. As part of the surrender terms, each district agreed to send one boy and one girl to appear in an annual televised event called, "The Hunger Games," a fight to the death on live TV. Sixteen-year-old Katniss Everdeen, who lives alone with her mother and younger sister, regards it as a death sentence when she is forced to represent her district in the Games. The terrain, rules, and level of audience participation may change but one thing is constant: kill or be killed.
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0545663261&ean=9780545663267"><h4>How Long to Read Mockingjay (The Final Book of The Hunger Games)</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/51ywQQjkppL.jpg">
+		<p>The third book in Suzanne Collins's phenomenal and worldwide bestselling Hunger Games trilogy is now available in paperback.
+
+"My name is Katniss Everdeen. Why am I not dead? I should be dead."
+
+Katniss Everdeen, girl on fire, has survived, even though her home has been destroyed. There are rebels. There are new leaders. A revolution is unfolding.
+
+District 13 has come out of the shadows and is plotting to overthrow the Capitol. Though she's long been a part of the revolution, Katniss hasn't known it. Now it seems that everyone has had a hand in the carefully laid plans but her.
+
+The success of the rebellion hinges on Katniss's willingness to be a pawn, to accept responsibility for countless....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0385737955&ean=9780385737951"><h4>How Long to Read The Maze Runner</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/51P9MJrBQCL.jpg">
+		<p>Read the first book in the #1 New York Times bestselling Maze Runner series, perfect for fans of The Hunger Games and Divergent. The Maze Runner is now a major motion picture featuring the star of MTV's Teen Wolf, Dylan O’Brien; Kaya Scodelario; Aml Ameen; Will Poulter; and Thomas Brodie-Sangster and the second book, The Scorch Trials, is soon to be a movie, hitting theaters September 18, 2015! Also look for James Dashner’s newest novels, The Eye of Minds and The Rule of Thoughts, the first two books in the Mortality Doctrine series.
+ 
+If you ain’t scared, you ain’t human.
+ 
+When Thomas wakes up in the lift, the only thing he can re....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0547928211&ean=9780547928210"><h4>How Long to Read The Fellowship of the Ring (The Lord of the Rings Series)</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/41i-SJkyCQL.jpg">
+		<p>The first volume in J.R.R. Tolkien's epic adventure THE LORD OF THE RINGS
+ 
+One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them
+
+In ancient times the Rings of Power were crafted by the Elven-smiths, and Sauron, the Dark Lord, forged the One Ring, filling it with his own power so that he could rule all others. But the One Ring was taken from him, and though he sought it throughout Middle-earth, it remained lost to him. After many ages it fell into the hands of Bilbo Baggins, as told in The Hobbit. In a sleepy village in the Shire, young Frodo Baggins finds himself faced with an immense task, as his elderly cousin ....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0547928203&ean=9780547928203"><h4>How Long to Read The Two Towers (The Lord of the Rings Series)</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/41OA4CHPoZL.jpg">
+		<p>The second volume in J.R.R. Tolkien's epic adventure THE LORD OF THE RINGS
+
+One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them
+
+Frodo and his Companions of the Ring have been beset by danger during their quest to prevent the Ruling Ring from falling into the hands of the Dark Lord by destroying it in the Cracks of Doom. They have lost the wizard, Gandalf, in a battle in the Mines of Moria. And Boromir, seduced by the power of the Ring, tried to seize it by force. While Frodo and Sam made their escape, the rest of the company was attacked by Orcs. Now they continue the journey alone down the great River Anduin—alone, that is,....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=054792819X&ean=9780547928197"><h4>How Long to Read The Return of the King (The Lord of the Rings Series)</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/51MlPWDaXGL.jpg">
+		<p>The third volume in J.R.R. Tolkien's epic adventure THE LORD OF THE RINGS 
+
+One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them
+
+As the Shadow of Mordor grows across the land, the Companions of the Ring have become involved in separate adventures. Aragorn, revealed as the hidden heir of the ancient Kings of the West, has joined with the Riders of Rohan against the forces of Isengard, and takes part in the desperate victory of the Hornburg. Merry and Pippin, captured by Orcs, escape into Fangorn Forest and there encounter the Ents. Gandalf has miraculously returned and defeated the evil wizard, Saruman. Sam has left his master ....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=054792822X&ean=9780547928227"><h4>How Long to Read The Hobbit</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/41aQPTCmeVL.jpg">
+		<p>Now a major motion picture
+
+A great modern classic and the prelude to THE LORD OF THE RINGS
+ 
+Bilbo Baggins is a hobbit who enjoys a comfortable, unambitious life, rarely traveling any farther than his pantry or cellar. But his contentment is disturbed when the wizard Gandalf and a company of dwarves arrive on his doorstep one day to whisk him away on an adventure. They have launched a plot to raid the treasure hoard guarded by Smaug the Magnificent, a large and very dangerous dragon. Bilbo reluctantly joins their quest, unaware that on his journey to the Lonely Mountain he will encounter both a magic ring and a frightening creature known as Gollum.
+
+“A glorious accou....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0062409859&ean=9780062409850"><h4>How Long to Read Go Set a Watchman</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/51JyC81DHoL.jpg">
+		<p>From Harper Lee comes a landmark new novel set two decades after her beloved Pulitzer Prize–winning masterpiece, To Kill a Mockingbird.
+Maycomb, Alabama. Twenty-six-year-old Jean Louise Finch—"Scout"—returns home from New York City to visit her aging father, Atticus. Set against the backdrop of the civil rights tensions and political turmoil that were transforming the South, Jean Louise's homecoming turns bittersweet when she learns disturbing truths about her close-knit family, the town, and the people dearest to her. Memories from her childhood flood back, and her values and assumptions are thrown into doubt. Featuring many of the iconic characters from To Kill a Mockingbird, Go Set....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0064404994&ean=9780064404990"><h4>How Long to Read The Lion, the Witch and the Wardrobe</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/51JrbSuPc9L.jpg">
+		<p>A beautiful paperback edition of The Lion, the Witch and the Wardrobe, book two in the classic fantasy series, The Chronicles of Narnia. This edition features cover art by three time Caldecott Medal-winning illustrator David Wiesner, and interior black-and-white illustrations by the series' original illustrator, Pauline Baynes.
+Four adventurous siblings—Peter, Susan, Edmund, and Lucy Pevensie—step through a wardrobe door and into the land of Narnia, a land frozen in eternal winter and enslaved by the power of the White Witch. But when almost all hope is lost, the return of the Great Lion, Aslan, signals a great change . . . and a great sacrifice.
+Open the door and enter a new world! ....
+</p>
+		<hr><a href="http://www.howlongtoreadthis.com/book_details.php?asin=0316769487&ean=9780316769488"><h4>How Long to Read The Catcher in the Rye</h4></a>
+		<img src="http://ecx.images-amazon.com/images/I/51namOub2kL.jpg">
+		<p>Anyone who has read J.D. Salinger's New Yorker stories ? particularly A Perfect Day for Bananafish, Uncle Wiggily in Connecticut, The Laughing Man, and For Esme ? With Love and Squalor, will not be surprised by the fact that his first novel is fully of children. The hero-narrator of THE CATCHER IN THE RYE is an ancient child of sixteen, a native New Yorker named Holden Caulfield. Through circumstances that tend to preclude adult, secondhand description, he leaves his prep school in Pennsylvania and goes underground in New York City for three days. The boy himself is at once too simple and too complex for us to make any final comment about him or his story. Perhaps the safest thing we can say about Holden is that he was b....
+</p>
+	</div>
+	
+	</p>
+	</div>
+	
+   </div>
+   </div>
+   </div>
+</section>
+<?php include_once('footer.php'); ?>
