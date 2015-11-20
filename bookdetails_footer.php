c9b838cc9a906a0f169a9b5b52c40bed2b70fa77
yayimahuman <donha@utschools.ca>
2015-11-19 19:17:03 -0500

    Create bookdetails_footer.php

---
+++ bookdetails_footer.php
@@ -0,0 +1,12 @@
+<div id="powered">
+	<div class="container">
+		<div class="copyright pull-left">  &copy; <?php echo date("Y"); ?>. Developed by Alexander Thorburn-Winsor. <a href="<?php
+  echo curPageURL(); ?>">How long to read <?php echo $arrBooksInfo->ItemAttributes->Title; ?></a></div>
+		
+		<center><iframe src="http://rcm-na.amazon-adsystem.com/e/cm?t=mechakeyboadv-20&o=1&p=48&l=ur1&category=audible&banner=0JYEMDNC49A58GM3J902&f=ifr&linkID=BJCNPQMEX5SGEFHA" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe></center><div class="pull-right"><a style="color:grey;" href="http://www.howlongtoreadthis.com/disclaimer.php"><span class="menu-title">Disclaimer</span></a></div>
+
+	</div>
+
+</div>
+</body>
+</html>
