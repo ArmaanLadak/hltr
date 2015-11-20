522110fa8842ebf35c90d8cb3be6d1972b688322
yayimahuman <donha@utschools.ca>
2015-11-19 19:22:06 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,40 @@
+<div id="mainnav">
+    <div class="container">
+      <div class="mainnav-inner">
+        <div class="row-fluid">
+        <div class="span6">
+       <h1 style="margin:0;"><a id="logo" href="http://www.howlongtoreadthis.com/index.php"><img width="34px" src="images/dwelling1.png"></a></h1>
+       
+        </div>
+          <nav id="mainmenu" class="span6">
+            <div class="navbar">
+              <div class="navbar-justified"> 
+              <a data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar"> <span class="icon-bar"></span> 
+              <span class="icon-bar"></span> <span class="icon-bar"></span> </a>
+              
+                <div class="nav-collapse collapse">
+                  <ul class="nav megamenu lui">
+                    
+                    <li class=""> <a href="http://www.howlongtoreadthis.com/index.php"><span class="menu-title">Home</span></a></li>
+
+                    <li class=""> <a href="http://howlongtoreadthis.com/howitworks.php"><span class="menu-title">How It Works</span></a></li>
+                    
+                    <li class=""> <a href="http://www.howlongtoreadthis.com/aboutus.php"><span class="menu-title">About</span></a></li>
+                  
+					<li class=""> <a href="http://www.howlongtoreadthis.com/contactus.php"><span class="menu-title">Contact</span></a></li>
+					<!--
+                    <li class="parent dropdown "> <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="menu-title">dropdown</span><b class="caret"></b></a>
+                      <div class="dropdown-menu level1">
+                        
+                      </div>
+                    </li>
+					-->
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
