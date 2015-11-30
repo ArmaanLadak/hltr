1b90f3f66ef90ed757c1bf02fc95df96e9f2afc2
yayimahuman <donha@utschools.ca>
2015-11-30 10:56:12 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <div class="footer" style="background:DarkSlateGrey">
 	<div class="container">
-		<div class="copyright pull-left">  &copy; 2015. Developed by Alexander Thorburn-Winsor </div> 
+		<div class="copyright pull-left" style="padding-top:18px;color:#F0F0F0">  &copy; 2015. Developed by Alex Thorburn-Winsor and Harry Dong </div> 
 	<div class="pull-right">
-		<a style="text-decoration:none" href="https://twitter.com/intent/tweet?text=Find%out%how%long%it%takes%you%to%read%any%book&url=http://www.howlongtoreadthis.com/" class="icon-button twitter"><i class="icon-twitter"></i><span></span></a>
-		<a style="text-decoration:none" href="https://www.facebook.com/sharer/sharer.php?u=http://www.howlongtoreadthis.com" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
+		<a style="text-decoration:none;padding:10px" href="https://twitter.com/intent/tweet?text=Find%out%how%long%it%takes%you%to%read%any%book&url=http://www.howlongtoreadthis.com/" class="icon-button twitter"><i class="icon-twitter"></i><span></span></a>
+		<a style="text-decoration:none;margin-top:10px" href="https://www.facebook.com/sharer/sharer.php?u=http://www.howlongtoreadthis.com" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
         </div>
 </div>
