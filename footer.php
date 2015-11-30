cdb81990cf784df28f858855f2f1ed67505d268d
yayimahuman <donha@utschools.ca>
2015-11-29 22:08:28 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,10 +3,8 @@
 	<div class="container">
 		<div class="copyright pull-left">  &copy; 2015. Developed by Alexander Thorburn-Winsor </div> 
 	<div class="pull-right">
-		<br>
-        	<span class='st_facebook_hcount' displayText='Facebook'></span>
-        	<span class='st_twitter_hcount' displayText='Tweet'></span>
-        	<span class='st_pinterest_hcount' displayText='Pinterest'></span>
+		<a style="text-decoration:none" href="https://twitter.com/intent/tweet?text=Find%out%how%long%it%takes%you%to%read%any%book&url=http://www.howlongtoreadthis.com/" class="icon-button twitter"><i class="icon-twitter"></i><span></span></a>
+		<a style="text-decoration:none" href="https://www.facebook.com/sharer/sharer.php?u=http://www.howlongtoreadthis.com" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
         </div>
 </footer>
 </div>
