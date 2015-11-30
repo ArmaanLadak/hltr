e3e616785da118411c74c0927d97e84229111b26
yayimahuman <donha@utschools.ca>
2015-11-29 22:20:37 -0500

    Update about.php

--- about.php
+++ about.php
@@ -6,61 +6,125 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta charset="UTF-8">
 <title>About Us</title>
-<link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="./css/stylesheet.css">
-<link rel="stylesheet" type="text/css" href="./css/bootstrap-responsive.css">
-<link rel="stylesheet" type="text/css" href="./css/theme-responsive.css">
-<script type="text/javascript" src="./js/jquery-1.7.1.min.js"></script>
-<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="http://www.howlongtoreadthis.com/css/stylesheet.css">
+<!--<link rel="stylesheet" type="text/css" href="http://www.howlongtoreadthis.com/css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css" href="http://www.howlongtoreadthis.com/css/theme-responsive.css">-->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script type="text/javascript" src="http://www.howlongtoreadthis.com/js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="http://www.howlongtoreadthis.com/js/bootstrap.min.js"></script>
 <link href='//fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-<script type="text/javascript">
-    $(function () {
-      $(document).ready(function(){
-        var Size = screen.height;
-			if(Size == 600)
-				{
-					  $(".mid_container").css('min-height',420);
-				}
-			if(Size == 768)
-				{
-					  $(".mid_container").css('min-height',664);
-				}
-			if(Size == 800)
-				{
-					  $(".mid_container").css('min-height',543);
-				}
-			if(Size == 832)
-				{
-					  $(".mid_container").css('min-height',563);
-				}	
-			if(Size == 900)
-				{
-					  $(".mid_container").css('min-height',800);
-				}	
-			if(Size == 1050)
-				{
-					  $(".mid_container").css('min-height',949);
-				}
-			if(Size == 1080)
-				{
-					  $(".mid_container").css('min-height',982);
-				}	
-				
-      });
-    });
-  </script>
+<link href="//fonts.googleapis.com/css?family=RobotoDraft:100,300,400,500,700" rel=
+    "stylesheet">
+<link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css'>
+<style>
+body{
+	background:#0CD39A;
+	font-family: 'Montserrat', serif, arial;
+}
+.navbar-default, .navbar-default .navbar-nav > li > a {
+    font-size: 14px;
+    text-shadow: none;
+    text-transform: uppercase;
+    background:white;
+    color:black;
+    -webkit-transition: all 0.3s ease;
+    -moz-transition: all 0.3s ease;
+    -o-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+}
+.navbar-default .navbar-nav > li > a:hover{
+    color:#0CD39A;
+}
+/* Wrapper */
+.icon-button {
+  background-color: white;
+  border-radius: 3.6rem;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 2.0rem;
+  height: 3.6rem;
+  line-height: 3.6rem;
+  margin: 0 5px;
+  position: relative;
+  text-align: center;
+  -webkit-user-select: none;
+     -moz-user-select: none;
+      -ms-user-select: none;
+          user-select: none;
+  width: 3.6rem;
+}
+
+/* Circle */
+.icon-button span {
+  border-radius: 0;
+  display: block;
+  height: 0;
+  left: 50%;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  -webkit-transition: all 0.3s;
+     -moz-transition: all 0.3s;
+       -o-transition: all 0.3s;
+          transition: all 0.3s;
+  width: 0;
+}
+.icon-button:hover span {
+  width: 3.6rem;
+  height: 3.6rem;
+  border-radius: 3.6rem;
+  margin: -1.8rem;
+}
+.twitter span {
+  background-color: #4099ff;
+}
+.facebook span {
+  background-color: #3B5998;
+}
+.google-plus span {
+  background-color: #db5a3c;
+}
+
+/* Icons */
+.icon-button i {
+  background: none;
+  color: white;
+  height: 3.6rem;
+  left: 0;
+  line-height: 3.6rem;
+  position: absolute;
+  top: 0;
+  -webkit-transition: all 0.3s;
+     -moz-transition: all 0.3s;
+       -o-transition: all 0.3s;
+          transition: all 0.3s;
+  width: 3.6rem;
+  z-index: 10;
+}
+.icon-button .icon-twitter {
+  color: #4099ff;
+}
+.icon-button .icon-facebook {
+  color: #3B5998;
+}
+.icon-button .icon-google-plus {
+  color: #db5a3c;
+}
+.icon-button:hover .icon-twitter,
+.icon-button:hover .icon-facebook,
+.icon-button:hover .icon-google-plus {
+  color: white;
+}
+</style>
 </head>
 <body class="fs12 page-home ">
-<div id="page-container">
+
 <?php include_once('header.php'); ?>
-</div>
-<section class="pav-showcase mid_container" id="pavo-showcase">
+
    <div class="container" >
    <div class="row-fluid" style="margin-bottom:20px; min-height:500px;">
    <div class="span12">
-	<div style="font-weight: bold; font-size: 25px; color:#ecf0f1;" class="span12 title">
-	About Us
-	</div>
+	<h2 style="color:white;">About Us</h2>
 	<div class="span12 title">
 	<br>
 	<div class="well well-small">
@@ -95,5 +159,7 @@ How Long to Read has been featured in Lifehacker.com, Yahoo! News, and other wel
    </div>
    </div>
    </div>
-</section>
+
 <?php include_once('footer.php'); ?>
+</body>
+</html>
