001ad694dc143abc2c32fabfc49e21a6a9a91245
yayimahuman <donha@utschools.ca>
2015-12-01 18:33:37 -0500

    Update search_header.php

--- search_header.php
+++ search_header.php
@@ -4,8 +4,9 @@
 			<div class="span5" style="text-align:left;">
 				<input type="text" style="height:34px;" name="search_keyword" id="search_keyword" value="<?php echo $searchKeyword; ?>">
 			</div>
+			
 			<div class="span4">
-				<?php if(isset($_GET['search_keyword'])){ >?
+				<?php if(isset($_GET['search_keyword'])){ ?>
 				<select name="any_category" id="any_category">
 					<option value="">All Categories</option>
 					<?php
@@ -18,6 +19,7 @@
 				</select>
 				<?php } ?>
 			</div>
+			
 			<div class="span3" style="text-align:center;"> 
 				<input type="submit" class="btn btn-default btn-large" value="Search">
 			</div>
