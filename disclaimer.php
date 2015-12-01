19dad1995232260b5068ca295dc758f72d0c141c
yayimahuman <donha@utschools.ca>
2015-12-01 10:33:19 -0500

    Update disclaimer.php

--- disclaimer.php
+++ disclaimer.php
@@ -6,35 +6,38 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta charset="UTF-8">
 <title>Disclaimer</title>
-<link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="./css/stylesheet.css">
-<link rel="stylesheet" type="text/css" href="./css/bootstrap-responsive.css">
-<link rel="stylesheet" type="text/css" href="./css/theme-responsive.css">
-<script type="text/javascript" src="./js/jquery-1.7.1.min.js"></script>
-<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <link href='//fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+<link href="//fonts.googleapis.com/css?family=RobotoDraft:100,300,400,500,700" rel=
+    "stylesheet">
+<link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css'>
+<link rel="stylesheet" type="text/css" href="./css/newstyles.css">
+
 </head>
-<body class="fs12 page-home ">
-<div id="page-container">
+
+<body>
+<div class="wrapper">
 <?php include_once('header.php'); ?>
+	<div class="container" >
+		<div class="row-fluid" style="margin-bottom:20px; min-height:500px;">
+			<div class="span12">
+				<h1 style="color:white">Disclaimer</h1>
+				<div class="span12 title">
+					<br>
+					<div class="well well-small">
+						<p style="text-align:justify;font-size:14px; color:#34495e; line-height:2">
+						How Long to Read makes money by providing links to buy books on Amazon and from other booksellers. When you want to buy a book that you've seen on our website and click on an Amazon link, we get a small commission if you buy the book, and it costs you nothing extra. This helps us keep the site up and running, and makes sure that we never have to sell your data to advertisers, along with providing easy access to the best prices on the web to buy books.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="push"></div>
 </div>
-<section class="pav-showcase mid_container" id="pavo-showcase">
-   <div class="container" >
-   <div class="row-fluid" style="margin-bottom:20px; min-height:500px;">
-   <div class="span12">
-	<div style="font-weight: bold; font-size: 25px; color:#ecf0f1;" class="span12 title">
-	Disclaimer
-	</div>
-	<div class="span12 title">
-	<br>
-	<div class="well well-small">
-	<p style="text-align:justify;font-size:14px; color:#34495e; line-height:2">
-	How Long to Read makes money by providing links to buy books on Amazon and from other booksellers. When you want to buy a book that you've seen on our website and click on an Amazon link, we get a small commission if you buy the book, and it costs you nothing extra. This helps us keep the site up and running, and makes sure that we never have to sell your data to advertisers, along with providing easy access to the best prices on the web to buy books.
-	</p>
-	</div>
-	</div>
-   </div>
-   </div>
-   </div>
-</section>
 <?php include_once('footer.php'); ?>
+</body>
+</html>
