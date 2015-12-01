2415caeb28a489962e4de59bca5019008751f933
yayimahuman <donha@utschools.ca>
2015-11-30 20:18:45 -0500

    finalized design

--- header.php
+++ header.php
@@ -1,41 +1,20 @@
-<!--
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<style>
-.navbar-default, .navbar-default .navbar-nav > li > a {
-    font-family: 'Montserrat', serif, arial;
-    font-size: 14px;
-    text-shadow: none;
-    text-transform: uppercase;
-    background:white;
-    color:black;
-    -webkit-transition: all 0.3s ease;
-    -moz-transition: all 0.3s ease;
-    -o-transition: all 0.3s ease;
-    transition: all 0.3s ease;
-}
-.navbar-default .navbar-nav > li > a:hover{
-    color:#0CD39A;
-}
-</style>
--->
-
 <nav class="navbar navbar-default navbar-static-top" style="margin:0px;border:none">
-  <div class="container-fluid"  style="max-width:1100px">
-    <div class="navbar-header">
-      <a style="margin-left:10px" class="navbar-brand" href="http://www.howlongtoreadthis.com/index.php"><img width="34px" src="images/dwelling1.png"></a>
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li> <a href="./index.php">Home</a></li>
-        <li> <a href="./howitworks.php">How It Works</a></li>
-        <li> <a href="./aboutus.php">About</a></li>
-	<li style="margin-right:10px"> <a href="./contactus.php">Contact</a></li>
-      </ul>
-    </div>
-  </div>
+	<div class="container-fluid"  style="max-width:1100px">
+		<div class="navbar-header">
+			<a style="margin-left:10px" class="navbar-brand" href="http://www.howlongtoreadthis.com/index.php"><img width="34px" src="images/dwelling1.png"></a>
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>                        
+			</button>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav navbar-right">
+				<li> <a href="./index.php">Home</a></li>
+				<li> <a href="./howitworks.php">How It Works</a></li>
+				<li> <a href="./aboutus.php">About</a></li>
+				<li style="margin-right:10px"> <a href="./contactus.php">Contact</a></li>
+			</ul>
+		</div>
+	</div>
 </nav>
