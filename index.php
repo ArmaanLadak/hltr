75b64d0db75fe2a2ae0adb7daf6135b21b730b92
yayimahuman <donha@utschools.ca>
2015-11-30 20:01:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,29 +9,14 @@
 <meta name="description" content="Search over 12 million books and find your reading time for each one with How Long to Read.">
 <meta charset="UTF-8">
 <title>How Long to Read</title>
-<!--<link rel="stylesheet" type="text/css" href="./css/stylesheet.css">
-<link rel="stylesheet" type="text/css" href="./css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css" href="./css/stylesheet.css">
+<!--<link rel="stylesheet" type="text/css" href="./css/bootstrap-responsive.css">
 <link rel="stylesheet" type="text/css" href="./css/theme-responsive.css">-->
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <link href='//fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 <link href="//fonts.googleapis.com/css?family=RobotoDraft:100,300,400,500,700" rel="stylesheet">
-<style>
-html, body {
-	height: 100%;
-}
-.wrapper {
-	min-height: 100%;
-	height: auto !important;
-	height: 100%;
-	margin: 0 auto -60px;
-}
-.footer, .push {
-	height: 60px;
-}
-
-</style>
 </head>
 
 <body>
