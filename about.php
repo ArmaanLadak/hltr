728c35dffe0d837a5f134b6751db45d9495208a3
yayimahuman <donha@utschools.ca>
2015-11-30 20:25:55 -0500

    Update about.php

--- about.php
+++ about.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en" class="off-canvas">
 <head>
-<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
-<script type="text/javascript">stLight.options({publisher: "042fe7c8-61a0-432b-9f9a-3808a2ef5415", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 <link rel="icon" href="/favicon.ico" type="image/x-icon">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<meta name="description" content="Search over 12 million books and find your reading time for each one with How Long to Read.">
 <meta charset="UTF-8">
 <title>About Us</title>
 
@@ -18,53 +15,47 @@
     "stylesheet">
 <link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css'>
 <link rel="stylesheet" type="text/css" href="./css/newstyles.css">
-
-</head>
 </head>
 
 <body>
 <div class="wrapper">
-
 <?php include_once('header.php'); ?>
-
-   <div class="container" >
-   <div class="row-fluid" style="margin-bottom:20px; min-height:500px;">
-   <div class="span12">
-	<h2 style="color:white;">About Us</h2>
-	<div class="span12 title">
-	<br>
-	<div class="well well-small">
-	<p style="text-align:justify;font-size:14px; color:#34495e; line-height:2">
-	How Long to Read is a book search engine that helps you find out how long it will take to read books and provide reading time data that is tailored to you. With our simple WPM (words per minute) test you can find out how long it will take you to read almost anything, and also use our search engine to find books that will fit the time you have to read.
-How Long to Read has been featured in Lifehacker.com, Yahoo! News, and other well known websites and blogs.
-	</p>
-	<hr>
-	<h3>Featured In:</h3>
-	<a href="https://www.yahoo.com/tech/s/8-amazing-websites-didn-t-know-existed-check-180019325.html" target="_blank"><img src="/yahoo.png" width="100" hspace="20" vspace="10"></a>
-	 
-	<a href="http://lifehacker.com/find-out-how-long-it-will-take-you-to-read-almost-any-b-1723514824" target="_blank"><img src="/lifehacker.png" width="100" hspace="20" vspace="10"></a>
-	
-	<a href="http://netted.net/extra-netted/calculate-how-long-a-book-will-take-to-read/" target="_blank"><img src="/logo-2.png" width="100" hspace="20" vspace="10"></a>
-	
-	<a href="http://www.independent.co.uk/arts-entertainment/books/news/find-out-how-long-it-will-take-you-to-read-the-classics-a6673326.html" target="_blank"><img src="/The_Independent_logo.png" width="140" hspace="20" vspace="10"></a>
-	
-	<a href="http://www.makeuseof.com/tag/5-tools-discovering-time-went/" target="_blank"><img src="/Makeuseoflogo.png" width="100" hspace="20" vspace="10"></a>
-	
-	<a href="http://www.post-gazette.com/ae/cybertainment/2015/08/28/Cybertainment-Site-offers-reading-times-for-books/stories/201508280120" target="_blank"><img src="/Gazette_logo.png" width="170" hspace="20" vspace="10"></a>
-	
-	<a href="http://www.bustle.com/articles/107201-how-long-does-it-take-to-read-moby-dick-this-website-tells-you-how-long-it-takes" target="_blank"><img src="/bustle.png" width="100" hspace="20" vspace="10"></a>
-	
-	<a href="http://wonderfulengineering.com/here-are-100-most-brilliant-tech-hacks-on-the-internet-part-2/" target="_blank"><img src="/Engineering-Website.png" width="100" hspace="20" vspace="10"></a>
-	
-	<a href="http://www.thememo.com/2015/08/14/use-howlongtoreadthis-to-pick-the-perfect-book-for-business-trips/" target="_blank"><img src="/thememo.png" width="100" hspace="20" vspace="10"></a>
-	
-	<a href="http://queleer.com.ve/2015/04/11/quieres-saber-cuanto-tardaras-en-leer-un-libro/" target="_blank"><img src="/queleerlogo1.png" width="100" hspace="20" vspace="10"></a>
-	
-	<a href="http://www.brit.co/how-long-to-read-this-website/" target="_blank"><img src="/britLogo.png" width="100" hspace="20" vspace="10"></a>
+<div class="container" >
+	<div class="row-fluid" style="margin-bottom:20px; min-height:500px;">
+		<div class="span12">
+			<h2 style="color:white;">About Us</h2>
+			<div class="span12 title">
+				<br>
+				<div class="well well-small">
+					<p style="text-align:justify;font-size:14px; color:#34495e; line-height:2">How Long to Read is a book search engine that helps you find out how long it will take to read books and provide reading time data that is tailored to you. With our simple WPM (words per minute) test you can find out how long it will take you to read almost anything, and also use our search engine to find books that will fit the time you have to read. How Long to Read has been featured in Lifehacker.com, Yahoo! News, and other well known websites and blogs.</p>
+					<hr>
+					<h3>Featured In:</h3>
+					<a href="https://www.yahoo.com/tech/s/8-amazing-websites-didn-t-know-existed-check-180019325.html" target="_blank"><img src="/yahoo.png" width="100" hspace="20" vspace="10"></a>
+					 
+					<a href="http://lifehacker.com/find-out-how-long-it-will-take-you-to-read-almost-any-b-1723514824" target="_blank"><img src="/lifehacker.png" width="100" hspace="20" vspace="10"></a>
+					
+					<a href="http://netted.net/extra-netted/calculate-how-long-a-book-will-take-to-read/" target="_blank"><img src="/logo-2.png" width="100" hspace="20" vspace="10"></a>
+					
+					<a href="http://www.independent.co.uk/arts-entertainment/books/news/find-out-how-long-it-will-take-you-to-read-the-classics-a6673326.html" target="_blank"><img src="/The_Independent_logo.png" width="140" hspace="20" vspace="10"></a>
+					
+					<a href="http://www.makeuseof.com/tag/5-tools-discovering-time-went/" target="_blank"><img src="/Makeuseoflogo.png" width="100" hspace="20" vspace="10"></a>
+					
+					<a href="http://www.post-gazette.com/ae/cybertainment/2015/08/28/Cybertainment-Site-offers-reading-times-for-books/stories/201508280120" target="_blank"><img src="/Gazette_logo.png" width="170" hspace="20" vspace="10"></a>
+					
+					<a href="http://www.bustle.com/articles/107201-how-long-does-it-take-to-read-moby-dick-this-website-tells-you-how-long-it-takes" target="_blank"><img src="/bustle.png" width="100" hspace="20" vspace="10"></a>
+					
+					<a href="http://wonderfulengineering.com/here-are-100-most-brilliant-tech-hacks-on-the-internet-part-2/" target="_blank"><img src="/Engineering-Website.png" width="100" hspace="20" vspace="10"></a>
+					
+					<a href="http://www.thememo.com/2015/08/14/use-howlongtoreadthis-to-pick-the-perfect-book-for-business-trips/" target="_blank"><img src="/thememo.png" width="100" hspace="20" vspace="10"></a>
+					
+					<a href="http://queleer.com.ve/2015/04/11/quieres-saber-cuanto-tardaras-en-leer-un-libro/" target="_blank"><img src="/queleerlogo1.png" width="100" hspace="20" vspace="10"></a>
+					
+					<a href="http://www.brit.co/how-long-to-read-this-website/" target="_blank"><img src="/britLogo.png" width="100" hspace="20" vspace="10"></a>
+				</div>
+			</div>
+		</div>
 	</div>
-   </div>
-   </div>
-   </div>
+</div>
 </div>
 <div class="push"></div>
 <?php include_once('footer.php'); ?>
