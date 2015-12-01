e15b1b47108b708b8cafaa1190f49f712d3874db
yayimahuman <donha@utschools.ca>
2015-12-01 16:48:26 -0500

    Update search_result.php

--- search_result.php
+++ search_result.php
@@ -58,182 +58,109 @@ if(isset($_GET['search_keyword'])){
 <link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css'>
 <link rel="stylesheet" type="text/css" href="./css/newstyles.css">
 
-<style>.btn-primary { padding: 4px 15px; } </style>
 </head>
 
 <body>
 <div class="wrapper" style="margin-bottom:1px">
 	<?php include_once('header.php'); ?>
-	<header id="header">
-		<div id="headerbottom">
-			<div class="container">
-				<div class="container-inner">
-					<form action="" method="get" id="filter_form">
-					<div class="row" style="margin-top:20px; margin-bottom:20px;">
-						<div class="span4" style="text-align:right;">
-							<input type="text" style="height:34px;" name="search_keyword" id="search_keyword" value="<?php echo $searchKeyword; ?>" class="span11">
-						</div>
-						<div class="span2">
-							<select name="condition" id="condition">
-								<option value="All">All</option>
-								<option value="New">New <?php if(isset($TotalNew)) echo $TotalNew; ?></option>
-								<option value="Used">Used <?php if(isset($TotalUsed)) echo $TotalUsed; ?></option>
-								<option value="Collectible">Collectible <?php if(isset($TotalCollectible)) echo $TotalCollectible; ?></option>
-								<option value="Refurbished">Refurbished <?php if(isset($TotalRefurbished)) echo $TotalRefurbished; ?></option>
-							</select>
-						</div>
-						<div class="span3">
-							<?php if(isset($_GET['search_keyword'])){ ?>
-							<select name="any_category" id="any_category">
-								<option value="">All Categories</option>
-								<?php
-								if(!empty($objSearchBins)){
-								foreach($objSearchBins as $arrBin){
-								?>
-								<option value="<?php echo $arrBin->BinParameter->Value; ?>"><?php echo $arrBin->BinName.' ('.$arrBin->BinItemCount.')'; ?></option>
-								<?php }
-								}?>
-							</select>
-							<?php } ?>
-						</div>
-						<div class="span2" style="text-align:center;"> 
-							<a class="btn btn-default btn-mini" href="#" style="margin-bottom:10px; " id="search_button">Search </a>
-						</div>
-					</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</header>
+	<?php include_once('search_header.php'); ?>
 
-<section style="background-image:url(Pav%20Decor_files/bg04.png); background-repeat:repeat;" class="pav-showcase mid_container" id="columns">
 	<div class="container">
-		<div class="row box-product">
-			<?php
-			if(isset($_GET['search_keyword'])){
-
-				$i	=	0;
-				if(!empty($objResult)){
+		<?php
+		if(isset($_GET['search_keyword'])){
+			$i	=	0;
+			if(!empty($objResult)){
 				foreach($objResult as $arrBooksInfo){ //print_r($arrBooksInfo);
 					if($i%3 == 0){ ?>
-				<div class="spacer10"></div>
-			<?php	} ?>
-			<div class="span4 product-block" style="width:31.203%">
-				<div class="product-inner">
-					<div class="image">
 						<br>
-						<a href="book_details.php?asin=<?php echo ((string)$arrBooksInfo->ASIN); ?>&ean=<?php echo ((string)$arrBooksInfo->ItemAttributes->EAN); ?>">
-							<img src="<?php echo $arrBooksInfo->LargeImage->URL; ?>" />
-						</a>
-					</div>
-					<div class="product-meta" style="color:#000;">
-						<div>
-							<div class="pull-left">
-							
+					<?php	} ?>
+					<a href="book_details.php?asin=<?php echo ((string)$arrBooksInfo->ASIN); ?>&ean=<?php echo ((string)$arrBooksInfo->ItemAttributes->EAN); ?>">
+						<div class="span4" style="background:white;padding:3px">
+							<div class="span4">
+								<img src="<?php echo $arrBooksInfo->LargeImage->URL; ?>" />
 							</div>
-							<br>
-							<a href="book_details.php?asin=<?php echo ((string)$arrBooksInfo->ASIN); ?>&ean=<?php echo ((string)$arrBooksInfo->ItemAttributes->EAN); ?>"><?php echo $arrBooksInfo->ItemAttributes->Title; ?></a>
-						</div>
-						<div class="clearfix"></div>
-						<div>
-							<div class="pull-left">
-							Author:
-							</div>
-							<div class="pull-right"><?php echo $arrBooksInfo->ItemAttributes->Author; ?></div>
-						</div>
-						<div class="clearfix"></div>
-						<div>
-							<div class="pull-left">
-							Number Of Pages:
-							</div>
-							<div class="pull-right"><?php echo $arrBooksInfo->ItemAttributes->NumberOfPages; ?></div>
-						</div>
-						<div class="clearfix"></div>
-						<div>
-							<div class="pull-left">
-							Number Of Words (Approximate):
-							</div>
-							<div class="pull-right"><?php echo $arrBooksInfo->ItemAttributes->NumberOfPages * 255; ?></div>
-						</div>
-						<div class="clearfix"></div>
-						<?php 
-							if(!empty($arrBooksInfo->ItemAttributes->Languages->Language)){
-							foreach($arrBooksInfo->ItemAttributes->Languages->Language as $arrLanguags){
-								if($arrLanguags->Type != 'Unknown'){ ?>							
+							<div class="span4">
+								<h4><?php echo $arrBooksInfo->ItemAttributes->Title; ?></h4>
 								<div>
 									<div class="pull-left">
-									<?php echo $arrLanguags->Type; ?>:
+									Author:
 									</div>
-									<div class="pull-right"><?php echo $arrLanguags->Name; ?></div>
+									<div class="pull-right"><?php echo $arrBooksInfo->ItemAttributes->Author; ?></div>
 								</div>
-								<div class="clearfix"></div>
-							<?php
-								}
-							} 
-							}
-						?>
-						
-						<div class="price" style="float:left; margin-right:10px;font-weight:bold;"> <?php echo ($arrBooksInfo->ItemAttributes->ListPrice->FormattedPrice != '')? $arrBooksInfo->ItemAttributes->ListPrice->FormattedPrice:'0.00'; ?> </div>
-						<div class="rating" style="float:left;">
-							<img alt="Based on 1 reviews." src="./images/stars-5.png">
+								<div>
+									<div class="pull-left">
+									Pages:
+									</div>
+									<div class="pull-right"><?php echo $arrBooksInfo->ItemAttributes->NumberOfPages; ?></div>
+								</div>
+								<div>
+									<div class="pull-left">
+									Words (Approximate):
+									</div>
+									<div class="pull-right"><?php echo $arrBooksInfo->ItemAttributes->NumberOfPages * 255; ?></div>
+								</div>
+								<div class="span4" style="float:left; margin-right:10px;font-weight:bold;"> 
+									<?php echo ($arrBooksInfo->ItemAttributes->ListPrice->FormattedPrice != '')? $arrBooksInfo->ItemAttributes->ListPrice->FormattedPrice:'0.00'; ?> 
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			</div>
-			<?php
-				$i++;
+					</a>
+					<?php
+					$i++;
 				}
-				}else{
+			}
+			else{
 					echo '<div class="alert alert-danger" role="alert">
-  					<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-  					<span class="sr-only"><b>Error:</b></span>
-  					No books were found that matched your search. Make sure you typed the title correctly. If you\'re seeing this often, the site is under heavy load and the server is not responding properly.
+					<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+					<span class="sr-only"><b>Error:</b></span>
+					No books were found that matched your search. Make sure you typed the title correctly. If you\'re seeing this often, it means the site is under heavy load and the server is not responding properly.
 					</div>';
 				}
-					$totalPages	 =	'';
-					$currUrl	=	$_SERVER['REQUEST_URI'];
-					$currUrl	=	explode('&', $currUrl);
-					$currPage	=	'';
-					$TotalPages	=	($TotalPages > 99)? 100:$TotalPages;
-				echo'<div class="myNav">';		
-						$i = 0;
-					foreach($currUrl as $arrExtVariables){	
-						if(strpos($arrExtVariables, 'page') !== false){
-							$currPage	=	isset($currUrl[$i])? filter_var($currUrl[$i], FILTER_SANITIZE_NUMBER_INT):'';
-							unset ($currUrl[$i]);
-						}
-						$i++;
+			$totalPages	 =	'';
+			$currUrl	=	$_SERVER['REQUEST_URI'];
+			$currUrl	=	explode('&', $currUrl);
+			$currPage	=	'';
+			$TotalPages	=	($TotalPages > 99)? 100:$TotalPages;
+			echo'<div class="myNav">';		
+			$i = 0;
+			foreach($currUrl as $arrExtVariables){	
+					if(strpos($arrExtVariables, 'page') !== false){
+						$currPage	=	isset($currUrl[$i])? filter_var($currUrl[$i], FILTER_SANITIZE_NUMBER_INT):'';
+						unset ($currUrl[$i]);
 					}
-				
-				if($TotalPages >= 15){
-							$totalPages = '';
-						if($currPage > 1){
-							$totalPages .= '<a class="active" href="'.implode('&', $currUrl).'&page=1">1</a>...';
-						}
+					$i++;
+				}
+			if($TotalPages >= 15){
+					$totalPages = '';
+					if($currPage > 1){
+						$totalPages .= '<a class="active" href="'.implode('&', $currUrl).'&page=1">1</a>...';
+					}
 					for($i=$currPage;$i<=($currPage+10);$i++){
 						//if($i < 10 || $i > ($TotalPages - 10)){
-							$totalPages	.=	'<a class="active" href="'.implode('&', $currUrl).'&page='.$i.'">'.$i.'</a> | ';
+						$totalPages	.=	'<a class="active" href="'.implode('&', $currUrl).'&page='.$i.'">'.$i.'</a> | ';
 						//}
 					}
-							$totalPages .= '...<a class="active" href="'.implode('&', $currUrl).'&page='.$TotalPages.'">'.$TotalPages.'</a>';
-						echo '<div class="spacer10"></div>'.$totalPages;
-				}else{
+					$totalPages .= '...<a class="active" href="'.implode('&', $currUrl).'&page='.$TotalPages.'">'.$TotalPages.'</a>';
+					echo '<div class="spacer10"></div>'.$totalPages;
+				}
+			else{
 					if($TotalPages > 1){
-							$totalPages = '';
-					for($i=1;$i<=($TotalPages);$i++){
-						//if($i < 10 || $i > ($TotalPages - 10)){
+						$totalPages = '';
+						for($i=1;$i<=($TotalPages);$i++){
+							//if($i < 10 || $i > ($TotalPages - 10)){
 							$totalPages	.=	'<a href="'.implode('&', $currUrl).'&page='.$i.'">'.$i.'</a> | ';
-						//}
-					}
-							echo '<div class="spacer10"></div>'.$totalPages;
+							//}
+						}
+						echo '<div class="spacer10"></div>'.$totalPages;
 					}
 				}
-				echo'</div>';
-			} ?>
-		</div>   
+			echo'</div>';
+		} ?>
 	</div>
-</section>
+	<div class="push"></div>
+</div>
 <?php include_once('footer.php'); ?>
+
 <script>
 $('document').ready(function(){
 	$('#search_button').click(function(){
@@ -241,3 +168,5 @@ $('document').ready(function(){
 	});
 });
 </script>
+</body>
+</html>
