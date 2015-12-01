2e50c11f15c121cc3d9d3d4046dc060edc9b09bf
yayimahuman <donha@utschools.ca>
2015-11-30 20:36:58 -0500

    finalized page

--- howitworks.php
+++ howitworks.php
@@ -25,28 +25,29 @@
 		<div class="span12">
 				<h1 style="color:white">How it Works</h1>
 				<div class="span12 title">
-				<br>
-				<div class="well well-small">
-				<p style="text-align:justify;font-size:14px; color:#34495e; line-height:2">
-				<b>1. </b>How Long to Read is a search engine that allows you to find almost any book and get your reading time for each one. 
-				Use the search bar on the home page to search for any of over 12 million books on How Long to Read.
-				<br><br>
-				<img src="HLTR_search.gif" alt="How to use How Long to Read">
-				<br>
-				<b><br>2. </b>On the search results page you'll find a general overview of all of the books matching your search term. Click on the book's cover image to go to its page and find your reading time for each one. 
-				<br><br>
-				<img src="howtouse1.png" alt="How to use How Long to Read">
-				<br><br>
-				<b>3. </b>Once you've found the book you're looking for on its page you will find the average readers reading time for the book, and on the right you will find a button to take a reading speed test and find your unique reading time for the book.
-				<br><br>
-				<img src="HLTR_test.gif" alt="How to use How Long to Read">
-				<br><br>
-				<b>4. </b>Once you're done reading click stop and in the popup your specific reading time will be displayed, along with your average reading speed. 
-				Once you've taken the reading speed test your reading time for the book will be saved in your browser, so if you ever want to check your time again your reading time will be saved and ready to view.
-				<br><br>
-				<img src="howtouse3.png" alt="How to use How Long to Read">
-			
-				</p>
+					<br>
+					<div class="well well-small">
+						<p style="text-align:justify;font-size:14px; color:#34495e; line-height:2">
+						<b>1. </b>How Long to Read is a search engine that allows you to find almost any book and get your reading time for each one. 
+						Use the search bar on the home page to search for any of over 12 million books on How Long to Read.
+						<br>
+						<br>
+						<img src="HLTR_search.gif" alt="How to use How Long to Read">
+						<br>
+						<b><br>2. </b>On the search results page you'll find a general overview of all of the books matching your search term. Click on the book's cover image to go to its page and find your reading time for each one. 
+						<br><br>
+						<img src="howtouse1.png" alt="How to use How Long to Read">
+						<br><br>
+						<b>3. </b>Once you've found the book you're looking for on its page you will find the average readers reading time for the book, and on the right you will find a button to take a reading speed test and find your unique reading time for the book.
+						<br><br>
+						<img src="HLTR_test.gif" alt="How to use How Long to Read">
+						<br><br>
+						<b>4. </b>Once you're done reading click stop and in the popup your specific reading time will be displayed, along with your average reading speed. 
+						Once you've taken the reading speed test your reading time for the book will be saved in your browser, so if you ever want to check your time again your reading time will be saved and ready to view.
+						<br><br>
+						<img src="howtouse3.png" alt="How to use How Long to Read">
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
