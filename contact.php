93d446cf703622b0cf9ba71f1e55561f9f91ed6e
yayimahuman <donha@utschools.ca>
2015-11-30 20:59:01 -0500

    Finalized design

--- contact.php
+++ contact.php
@@ -27,112 +27,56 @@ if(isset($_POST['submit'])){
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta charset="UTF-8">
 <title>Contact Us</title>
-<link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="./css/stylesheet.css">
-<link rel="stylesheet" type="text/css" href="./css/bootstrap-responsive.css">
-<link rel="stylesheet" type="text/css" href="./css/theme-responsive.css">
-<script type="text/javascript" src="./js/jquery-1.7.1.min.js"></script>
-<script type="text/javascript" src="./js/bootstrap.min.js"></script>
-<link href='//fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-</head>
-<body class="fs12 page-home ">
-<div id="page-container">
-<?php include_once('header.php'); ?>
-</div>
-<section class="pav-showcase mid_container" id="pavo-showcase">
-   <div class="container" >
-   <div class="row-fluid" style="margin-bottom:20px; min-height:500px;">
-   <div class="span12">
-	<div style="font-weight: bold; font-size: 25px; color:#ecf0f1;" class="span12 title">
-	Contact Us
-	</div>
-	
-	<div class="span12 title">
-	<?php
-		if(isset($_SESSION['sent_mail'])){
-			echo($_SESSION['sent_mail'].'<br>');
-			unset($_SESSION['sent_mail']);
-		}
-	?>
-	<br>
 
-	<div class="alert">
-	If you have any questions/comments about the site or would like to partner with us please contact us using the form below.
-	</div>
-	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	<div style="margin:0px!important; padding:0px !important;" class="span12">
-		<div class="span10">
-			<div class="span12">
-				<div class="span4">
-					<label style="font-size:14px">Name</label>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link href='//fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+<link href="//fonts.googleapis.com/css?family=RobotoDraft:100,300,400,500,700" rel=
+    "stylesheet">
+<link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css'>
+<link rel="stylesheet" type="text/css" href="./css/newstyles.css">
+</head>
+
+<body>
+<div class="wrapper">
+	<?php include_once('header.php'); ?>
+	   <div class="container" >
+		   <div class="row-fluid" style="margin-bottom:20px; min-height:500px;">
+			   <div class="span12">
+				<h1 style="color:white">Contact Us</h1>
+				<div class="span12 title">
+					<?php
+						if(isset($_SESSION['sent_mail'])){
+							echo($_SESSION['sent_mail'].'<br>');
+							unset($_SESSION['sent_mail']);
+						}
+					?>
+					<br>
+					<div class="alert alert-warning">If you have any questions/comments about the site or would like to partner with us please contact us using the form below.</div>
+					<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+						<div class="form-group">
+							<label for="name">Name:</label>
+							<input type="text" class="form-control" name="name" placeholder="Your Name">
+						</div>
+						<div class="form-group">
+							<label for="email">Email:</label>
+							<input type="email" class="form-control" name="email" placeholder="Your Email">
+						</div>
+						<div class="form-group">
+							<label for="subject">Subject:</label>
+							<input type="text" class="form-control" name="subject" placeholder="Enter password">
+						</div>
+						<div class="form-group">
+							<label for="message">Message:</label>
+							<textarea class="form-control" rows="5" name="message"></textarea>
+						</div>
+						<input type="submit" class="btn btn-default" name="submit" value="Submit">
+					</form>
 				</div>
-				<div class="span8">
-					<input style="height:20px padding:5px; border:2px solid #ccc; 
--webkit-border-radius: 5px;
-border-radius: 5px; !important;" type="text" name="name">
-				</div>	
 			</div>
 		</div>
-		<div class="span2"></div>
 	</div>
-	<div style="margin:0px!important; padding:0px !important;" class="span12">
-		<div class="span10">
-			<div class="span12">
-				<div class="span4">
-					<label style="font-size:14px">Email</label>
-				</div>
-				<div class="span8">
-					<input style="height:20px padding:5px; border:2px solid #ccc; 
--webkit-border-radius: 5px;
-border-radius: 5px; !important;" type="text" name="email">
-				</div>	
-			</div>
-		</div>	
-		<div class="span2"></div>
-	</div>
-	<div style="margin:0px!important; padding:0px !important;" class="span12">
-		<div class="span10">
-			<div class="span12">
-				<div class="span4">
-					<label style="font-size:14px">Subject</label>
-				</div>
-				<div class="span8">
-					<input style="height:20px padding:5px; border:2px solid #ccc; 
--webkit-border-radius: 5px;
-border-radius: 5px; !important;" type="text" name="subject">
-				</div>	
-			</div>
-		</div>	
-		<div class="span2"></div>
-	</div>
-	<div style="margin:0px!important; padding:0px !important;" class="span12">
-		<div class="span10">
-			<div class="span12">
-				<div class="span4">
-					<label style="font-size:14px">Message</label>
-				</div>
-				<div class="span8">
-					<textarea width="400px" height="300px" name="message" style="padding:5px; border:2px solid #ccc; 
--webkit-border-radius: 5px;
-border-radius: 5px;"></textarea>
-				</div>	
-			</div>
-		</div>	
-		<div class="span2"></div>
-	</div>
-	<div style="margin:0px!important; padding:0px !important;" class="span12">
-		<div class="span10">
-			<input type="submit" name="submit" value="Submit" style="padding:5px; border:2px solid #ccc; 
--webkit-border-radius: 5px;
-border-radius: 5px;">
-		</div>	
-		<div class="span2"></div>
-	</div>
-	</form>
-	</p>
-	</div>
-   </div>
-   </div>
-   </div>
-</section>
+	<div class="push"></div>
+</div>
 <?php include_once('footer.php'); ?>
