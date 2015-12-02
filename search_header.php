d6fb2ce053917f8cb5077807608bdd81afa5bf9b
yayimahuman <donha@utschools.ca>
2015-12-01 22:22:26 -0500

    Finalized design

--- search_header.php
+++ search_header.php
@@ -1,28 +1,10 @@
-<div class="container">
-	<form action="search_result.php" method="get" id="filter_form">
-		<div class="row" style="margin-top:20px; margin-bottom:20px;">
-			<div class="span5" style="text-align:left;">
-				<input type="text" style="height:34px;" name="search_keyword" id="search_keyword" value="<?php echo $searchKeyword; ?>">
-			</div>
-			
-			<div class="span4">
-				<?php if(isset($_GET['search_keyword'])){ ?>
-				<select name="any_category" id="any_category">
-					<option value="">All Categories</option>
-					<?php
-					if(!empty($objSearchBins)){
-						foreach($objSearchBins as $arrBin){
-							?>
-							<option value="<?php echo $arrBin->BinParameter->Value; ?>"><?php echo $arrBin->BinName.' ('.$arrBin->BinItemCount.')'; ?></option>
-						<?php }
-					}?>
-				</select>
-				<?php } ?>
-			</div>
-			
-			<div class="span3" style="text-align:center;"> 
-				<input type="submit" class="btn btn-default btn-large" value="Search">
-			</div>
-		</div>
+<div class="row">
+	<form action="search_result.php" method="get" id="filter_form" class="form-inline" role="form" style="margin:8px">
+		<center><div class="col-sm-9" style="margin:5px 15px 5px 15px">
+			<input type="text" class="form-control" name="search_keyword" id="search_keyword" value="<?php echo $searchKeyword; ?>" placeholder="Search" style="width:100%">
+		</div></center>
+		<center><div class="col-sm-2" style="margin:5px 0px 5px 0px">
+			<input type="submit" class="btn btn-default btn-large" value="Search">
+		</div></center>
 	</form>
 </div>
