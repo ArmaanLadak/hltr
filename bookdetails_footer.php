85c316fc14588084ac056738895936215a1fe70c
yayimahuman <donha@utschools.ca>
2015-12-04 18:58:22 -0500

    Finalized design

--- bookdetails_footer.php
+++ bookdetails_footer.php
@@ -1,12 +1,17 @@
-<div id="powered">
+<div class="footer-details" style="background:DarkSlateGrey">
 	<div class="container">
-		<div class="copyright pull-left">  &copy; <?php echo date("Y"); ?>. Developed by Alexander Thorburn-Winsor. <a href="<?php
-  echo curPageURL(); ?>">How long to read <?php echo $arrBooksInfo->ItemAttributes->Title; ?></a></div>
-		
-		<center><iframe src="http://rcm-na.amazon-adsystem.com/e/cm?t=mechakeyboadv-20&o=1&p=48&l=ur1&category=audible&banner=0JYEMDNC49A58GM3J902&f=ifr&linkID=BJCNPQMEX5SGEFHA" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe></center><div class="pull-right"><a style="color:grey;" href="http://www.howlongtoreadthis.com/disclaimer.php"><span class="menu-title">Disclaimer</span></a></div>
-
+		<div id="credits" class="copyright pull-left" style="padding-top:20px;color:#F0F0F0">  
+			&copy; 2015. Developed by Alex Thorburn-Winsor and Harry Dong 
+		</div> 
+		<div class="pull-right">
+			<a style="text-decoration:none;padding:10px" href="https://twitter.com/intent/tweet?text=Find%out%how%long%it%takes%you%to%read%any%book&url=http://www.howlongtoreadthis.com/" class="icon-button twitter"><i class="icon-twitter"></i><span></span></a>
+			<a style="text-decoration:none;margin-top:10px" href="https://www.facebook.com/sharer/sharer.php?u=http://www.howlongtoreadthis.com" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
+		</div>
 	</div>
-
+	<div class="container" style="overflow:hidden">
+	    <center><iframe src="http://rcm-na.amazon-adsystem.com/e/cm?t=mechakeyboadv-20&o=1&p=48&l=ur1&category=audible&banner=0JYEMDNC49A58GM3J902&f=ifr&linkID=BJCNPQMEX5SGEFHA" style="width:728px;height:90px;overflow:hidden;border:none;margin:0;"></iframe></center>
+	    <div class="pull-right">
+	    	<a style="color:grey;" href="http://www.howlongtoreadthis.com/disclaimer.php">Disclaimer</a>
+	    </div>
+    </div>
 </div>
-</body>
-</html>
